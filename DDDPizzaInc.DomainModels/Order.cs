5798796db2c1ddcef4c2591dc1a3966e38e98c47
akhelifa <akhelifa@hotmail.com>
2016-10-26 11:31:25 +0100

    Added some untracked entities

---
+++ DDDPizzaInc.DomainModels/Order.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Events;
+using DDDPizzaInc.DomainModels.Events;
+using DDDPizzaInc.SharedKernel;
+
+namespace DDDPizzaInc.DomainModels
+{
+    public class Order : Entity
+    {
+        public Order(ServiceType service, List<Pizza> pizzas, string name)
+        {
+            Guard.ForNullOrEmpty(name, "Customer Name must be provided");
+            Name = name;
+            ServiceType = service;
+            Pizzas = pizzas;
+            CalculateTotal();
+            DateTimeStamp = DateTime.UtcNow;
+        }
+        public string Name { get; set; }
+        public ServiceType ServiceType { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public DateTime DateTimeStamp { get; private set; }
+        public decimal SubTotal { get; private set; }
+        public decimal ServiceCharge { get; private set; }
+        public decimal TotalAmount { get; private set; }
+        public DateTime EstimatedReadyTime { get; private set; } // 20 mins + active = 20, 24
+
+        private void CalculateTotal()
+        {
+            foreach (var order in Pizzas)
+            {
+                SubTotal += order.Total;
+            }
+
+            ServiceCharge = ServiceType.CalculateTotal(this.ServiceType);
+
+            TotalAmount = SubTotal + ServiceCharge;
+
+        }
+
+        public void SetEstimatedReadyTime(long existingOrders)
+        {
+            EstimatedReadyTime = DateTime.UtcNow.AddMinutes(20).AddMinutes(existingOrders * 2);
+        }
+
+        public void ProcessOrder(Order order, IEventPublisher eventPublisher)
+        {
+
+            if (Equals(order.ServiceType, ServiceType.Delivery))
+            {
+                eventPublisher.Publish(new OrderNeedsDelivery(this));
+            }
+        }
+    }
+} 
---
+++ DDDpizzaInc.UnitTests/PizzaUnitTests.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Events;
+using DDDPizzaInc.DomainModels;
+using DDDPizzaInc.DomainModels.Events;
+using DDDPizzaInc.Mocks;
+using Moq;
+using NUnit.Framework;
+
+namespace DDDpizzaInc.UnitTests
+{
+    [TestFixture, Category("UnitTests")]
+    public class PizzaUnitTests
+    {
+        private Mock<IEventPublisher> _mockEventPublisher;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockEventPublisher = new Mock<IEventPublisher>(MockBehavior.Strict);
+        }
+
+        #region " INVENTORY TESTS "
+
+        [Test]
+        public void Should_Create_Instance_Of_Bread()
+        {
+            // Arrange
+            const string name = "Double Crust";
+
+            //Act
+            var sut = new Bread(name);
+            var serialize = sut.ShouldSerializePrice();
+
+            //Assert
+            Assert.IsInstanceOf<Bread>(sut);
+            Assert.IsNotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(0, sut.Price);
+            Assert.AreEqual(name, sut.Name);
+            Assert.AreEqual(false, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Bread_With_Guid()
+        {
+            // Arrange
+            const string name = "Double Crust";
+            var id = Guid.NewGuid();
+ 
+            // Act
+            var sut = new Bread(id, name);
+            var serialize = sut.ShouldSerializePrice();
+
+            // Assert
+            Assert.IsInstanceOf<Bread>(sut);
+            Assert.IsNotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(0, sut.Price);
+            Assert.AreEqual(name, sut.Name);
+            Assert.AreEqual(false, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Cheese()
+        {
+            const string name = "Mozzarella";
+            var sut = new Cheese(name);
+            var serialize = sut.ShouldSerializePrice();
+            Assert.IsInstanceOf<Cheese>(sut);
+            Assert.IsNotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(0, sut.Price);
+            Assert.AreEqual(name, sut.Name);
+            Assert.AreEqual(false, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Cheese_With_Guid()
+        {
+            const string name = "Mozzarella";
+            var id = Guid.NewGuid();
+            var sut = new Cheese(id, name);
+            var serialize = sut.ShouldSerializePrice();
+            Assert.IsInstanceOf<Cheese>(sut);
+            Assert.IsNotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(0, sut.Price);
+            Assert.AreEqual(name, sut.Name);
+            Assert.AreEqual(false, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Sauce()
+        {
+            const string name = "Pesto";
+            var sut = new Sauce(name);
+            var serialize = sut.ShouldSerializePrice();
+            Assert.IsInstanceOf<Sauce>(sut);
+            Assert.IsNotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(0, sut.Price);
+            Assert.AreEqual(name, sut.Name);
+            Assert.AreEqual(false, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Sauce_With_Guid()
+        {
+            var id = Guid.NewGuid();
+            const string name = "Pesto";
+            var sut = new Sauce(id, name);
+            var serialize = sut.ShouldSerializePrice();
+            Assert.IsInstanceOf<Sauce>(sut);
+            Assert.IsNotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(0, sut.Price);
+            Assert.AreEqual(name, sut.Name);
+            Assert.AreEqual(false, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Size()
+        {
+            // Arrange
+            const decimal price = 23.11m;
+            
+            // Act
+            var sut = new Size("reallyBig!", price);
+            var serialize = sut.ShouldSerializePrice();
+            
+            // 
+            Assert.IsInstanceOf<Size>(sut);
+            Assert.NotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(price, sut.Price);
+            Assert.AreEqual(true, serialize);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Size_With_Guid()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            const decimal price = 23.11m;
+
+            // Act
+            var sut = new Size(id, "reallyBig!", price);
+            var serialize = sut.ShouldSerializePrice();
+
+            // Assert
+            Assert.IsInstanceOf<Size>(sut);
+            Assert.NotNull(sut.Id);
+            Assert.IsInstanceOf<Guid>(sut.Id);
+            Assert.AreEqual(price, sut.Price);
+            Assert.AreEqual(true, serialize);
+        }
+
+        #endregion
+
+        [Test]
+        public void Should_Create_Instance_Of_Pizza_Calculate_Total()
+        {
+            // Arrange
+            var toppingsCost = PizzaMocks.ToppingMocks().Sum(x => x.Price);
+            var sizeCost = PizzaMocks.SizeMocks().First().Price;
+            var expectedCost = toppingsCost + sizeCost;
+
+            // Act
+            var sut = new Pizza(
+                                PizzaMocks.ToppingMocks().ToList(), 
+                                PizzaMocks.SizeMocks().First(), 
+                                PizzaMocks.BreadMocks().ElementAt(1), 
+                                PizzaMocks.GetSauces().ElementAt(1), 
+                                PizzaMocks.GetCheese().ElementAt(2));
+
+            // Assert
+            Assert.IsInstanceOf<Pizza>(sut);
+            Assert.Less( 0, sut.Total);
+            Assert.AreEqual(expectedCost, sut.Total);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Order_With_Delivery()
+        {
+            // Arrange
+            _mockEventPublisher.Setup(x => x.Publish(It.IsAny<IDomainEvent>())).Callback(() =>
+            {
+                new OrderNeedsDelivery(It.IsAny<Order>());
+            });
+
+            // Act
+            var pizza = new Pizza(PizzaMocks.ToppingMocks().ToList(), PizzaMocks.SizeMocks().First(), PizzaMocks.BreadMocks().ElementAt(1), PizzaMocks.GetSauces().ElementAt(1), PizzaMocks.GetCheese().ElementAt(2));
+            var pizzas = new List<Pizza>() { pizza };
+            var sut = new Order(ServiceType.Delivery, pizzas, "Jose");
+            sut.ProcessOrder(sut, _mockEventPublisher.Object);
+
+            // Assert
+            _mockEventPublisher.VerifyAll();
+            _mockEventPublisher.Verify(x => x.Publish(It.IsAny<IDomainEvent>()), Times.Once);
+            Assert.IsInstanceOf<Order>(sut);
+            Assert.Greater(sut.ServiceCharge, 0);
+            Assert.Greater(sut.SubTotal, 0);
+            Assert.AreEqual(sut.ServiceCharge + sut.SubTotal, sut.TotalAmount);
+        }
+
+        [Test]
+        public void Should_Create_Instance_Of_Order_With_InRestaurant()
+        {
+            var pizza = new Pizza(PizzaMocks.ToppingMocks().ToList(), PizzaMocks.SizeMocks().First(), PizzaMocks.BreadMocks().ElementAt(1), PizzaMocks.GetSauces().ElementAt(1), PizzaMocks.GetCheese().ElementAt(2));
+            var pizzas = new List<Pizza>() { pizza };
+            var sut = new Order(ServiceType.InRestaurant, pizzas, "Jose");
+            sut.ProcessOrder(sut, _mockEventPublisher.Object);
+
+            _mockEventPublisher.VerifyAll();
+            _mockEventPublisher.Verify(x => x.Publish(It.IsAny<IDomainEvent>()), Times.Never);
+            Assert.IsInstanceOf<Order>(sut);
+            Assert.AreEqual(sut.ServiceCharge, 0);
+            Assert.Greater(sut.SubTotal, 0);
+            Assert.AreEqual(sut.ServiceCharge + sut.SubTotal, sut.TotalAmount);
+        }
+
+
+        [Test]
+        public void Should_Create_Instance_Of_Order_With_TakeOut()
+        {
+            var pizza = new Pizza(PizzaMocks.ToppingMocks().ToList(), PizzaMocks.SizeMocks().First(), PizzaMocks.BreadMocks().ElementAt(1), PizzaMocks.GetSauces().ElementAt(1), PizzaMocks.GetCheese().ElementAt(2));
+            var pizzas = new List<Pizza>() { pizza };
+            var sut = new Order(ServiceType.TakeOut, pizzas, "Jose");
+            sut.ProcessOrder(sut, _mockEventPublisher.Object);
+
+            _mockEventPublisher.VerifyAll();
+            _mockEventPublisher.Verify(x => x.Publish(It.IsAny<IDomainEvent>()), Times.Never);
+            Assert.IsInstanceOf<Order>(sut);
+            Assert.AreEqual(sut.ServiceCharge, 0);
+            Assert.Greater(sut.SubTotal, 0);
+            Assert.AreEqual(sut.ServiceCharge + sut.SubTotal, sut.TotalAmount);
+        }
+
+    }
+}
